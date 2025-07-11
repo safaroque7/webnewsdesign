9377dc316765ebae6c7fec9641fdafe9c60032ca
S A Faroque <faroque.computer@gmail.com>
2025-07-11 18:43:15 +0600

    changed code line

--- page.php
+++ page.php
@@ -1,9 +1,11 @@
-<?php get_header(); ?>
+<?php
+get_header();
+get_template_part('include/breadcrumb-design');
+?>
 <!--start wrap-->
 <div class="container">
     <!--start category_and_other-->
     <div class="row mb-2 mb-md-5">
-        <?php get_template_part('include/breadcrumb-design'); ?>
         <!--start per_templat_matter-->
         <div class="col-12 bg-white pt-3 pages-ul-li">
             <?php
