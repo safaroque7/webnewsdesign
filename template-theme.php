f4385497ce2345e0531bdc79b5e4b6deb8ca7252
S A Faroque <faroque.computer@gmail.com>
2025-07-30 12:28:48 +0600

    bugfix/themes to theme

--- template-theme.php
+++ template-theme.php
@@ -1,13 +1,11 @@
 <?php
 /*
-Template Name: Themes
+Template Name: Theme
 */
 get_header();
 include_once('include/breadcrumb-design.php');
 ?>
 <!-- *****Header Section End***** -->
-
-
 
 <div class="container-fluid">
     <div class="container">
@@ -15,18 +13,19 @@ include_once('include/breadcrumb-design.php');
 
 
             <?php
-        $args = array(
-            'post_type'         => 'themes', // if you want to further filter by post_type
-            'order'             => 'ASC',
-            'posts_per_page'    =>  -1
-        );
-        $query = new WP_Query($args);
+            $args = array(
+                'post_type'         => 'themes', // if you want to further filter by post_type
+                'order'             => 'ASC',
+                'posts_per_page'    =>  -1
+            );
+            $query = new WP_Query($args);
 
-        //  start quering
-        while ($query->have_posts()): $query->the_post(); 
-        get_template_part('part/theme-query');
-        endwhile; wp_reset_postdata(); 
-      ?>
+            //  start quering
+            while ($query->have_posts()): $query->the_post();
+                get_template_part('part/theme-query');
+            endwhile;
+            wp_reset_postdata();
+            ?>
 
         </div>
     </div>
